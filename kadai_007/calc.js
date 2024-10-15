607b1163c12fcd5c905d163d547d81fbbe2b4048
210rabbits <hanayo.tojyo@gmail.com>
2024-10-15 13:22:06 +0900

    ファイルを追加

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,12 +1,12 @@
-let num = 24;
-if (num % 3 === 0) {
-    console.log('3の倍数です');
+let num = 5;
+if (num % 3 === 0 && num % 5 === 0) {
+    console.log('3と5の倍数です');
 }
 else if (num % 5 === 0) {
     console.log('5の倍数です');
 }
-else if (num % 3 === 0 && num % 5 === 0) {
-    console.log('3と5の倍数です');
+else if (num % 3 === 0) {
+    console.log('3の倍数です');
 }
 else {
     console.log(num);
